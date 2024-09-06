f58b4271e35a12de0e506b73d0124967c29661fc
Mikaelly Pardinho <168122231+MikaPardinho01@users.noreply.github.com>
2024-09-06 15:10:02 -0300

    teste

--- Complet_code/src/portao.cpp
+++ Complet_code/src/portao.cpp
@@ -1,6 +1,6 @@
-#include <Arduino.h>
 #include <Servo.h>
 #include <Bounce2.h>
+#include "portao.h"
 
 #define pinServo 15
 #define pinBotao 22
@@ -11,21 +11,21 @@ Bounce botao = Bounce();
 bool ativacao = false;
 bool servoInicio = false;
 
-void setup()
+void inicializa_portao ()
 {
-  botao.attach(pinBotao, INPUT_PULLUP);
+    botao.attach(pinBotao, INPUT_PULLUP);
 
-  Serial.begin(9600);
-  spinning.attach(pinServo);
+    Serial.begin(9600);
+    spinning.attach(pinServo);
 
-  spinning.write(pinServo, 90); 
-  delay(500);
-  servoInicio = true;
+    spinning.write(pinServo, 90); 
+    delay(500);
+    servoInicio = true;
 }
 
-void loop()
+void posiciona_portao() 
 {
-  botao.update();
+      botao.update();
   if (botao.changed() && botao.read() == HIGH) 
   {
     ativacao = !ativacao; 
@@ -41,6 +41,3 @@ void loop()
     }
   }
 }
-
-
-// bom diaaaaaaaaaaaaaaaaaaaaaa
