3b3e3dd2fad3f6ccad10441f9ac2562ead2d1b7f
Mikaelly Pardinho <168122231+MikaPardinho01@users.noreply.github.com>
2024-10-28 17:18:28 -0300

    Display2024

--- Display_2.0/src/main.cpp
+++ Display_2.0/src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <Bounce2.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PIN_BOTAO 0
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Bounce botao = Bounce();
+LiquidCrystal_I2C lcd(0x27, 28, 4);
+
+bool acctive = false;
+
+void setup()
+{
+  lcd.init(); // Inicializa o LCD
+  lcd.backlight(); // inicializa a luz de fundo
+  lcd.setCursor(0, 0); // posiciona o cursor na coluna 0 e linha 0
+  lcd.print("Hello, World!"); // Escreve 
+
+  // Primeiro posiciona o cursor, depois o texto
+  lcd.setCursor(0, 1); 
+  lcd.print("Mika");
+  lcd.setCursor(0, 2); 
+  lcd.print("Senai");
+  lcd.setCursor(0, 3); 
+  lcd.print("2024");
+  delay(3000);  // aguarda 3 segundos (trocar por millis)
+  lcd.clear(); // limpa a tela
+
+  botao.attach(PIN_BOTAO, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  botao.update();
+  if (botao.fell()) 
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Botao press");
+  }
+  else if (botao.rose())
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Botao nao press");
+  }
 }
