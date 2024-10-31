c7ba93d5138eabdd1ce5acdbe897a6b3c82d1963
Mikaelly Pardinho <168122231+MikaPardinho01@users.noreply.github.com>
2024-10-31 15:50:15 -0300

    teste do sensor motor de passo

--- Esteira/src/main.cpp
+++ Esteira/src/main.cpp
@@ -2,57 +2,51 @@
 #include <Arduino.h>
 #include <Stepper.h> // incluir motor de passo
 
-const int stepsPerRevolution = 2048; // é o número de passos que um motor de passo deve dar para completar uma volta completa de 360 graus.
+const int stepsPerRevolution = 2048; // número de passos para uma volta completa de 360 graus
 
 // *===== DEFINICOES ======
-// Pins setados no motor de passo
 #define IN1 19
 #define IN2 18
 #define IN3 5
 #define IN4 17
+#define BUTTON_PIN 0
 
 // *===== OBJETOS ========
-// inicializar a biblioteca do motor (stepper)
 Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 
 unsigned long previousMillis = 0; // variável para armazenar o tempo anterior
 const long interval = 1000; // intervalo de tempo em milissegundos (1 segundo)
 
 bool motorLigado = false; // variável para rastrear o estado do motor
+bool buttonState = false; // estado do botão
+bool lastButtonState = false; // último estado do botão
 
-void inicializa_motor() 
-{
-  // define a velocidade em 5 rpm (é uma medida de quantas voltas completas um objeto ou motor faz em um minuto.)
+void inicializa_motor() {
+  // define a velocidade em 5 rpm
   myStepper.setSpeed(5);
   Serial.begin(115200);
   Serial.println("Motor desligado");
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Configura o botão como entrada com resistor de pull-up interno
 }
 
-void atualiza_motor() 
-{
-  unsigned long currentMillis = millis(); // obtém o tempo atual em milissegundos
+void atualiza_motor() {
+  unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) 
-  {
-    // salva o tempo atual
+  if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
 
-    if (motorLigado) 
-    {
-      // passo uma revolução em uma direção:
+    if (motorLigado) {
       Serial.println("Motor ligado - sentido horário");
       myStepper.step(stepsPerRevolution);
-    }
-    else 
-    {
+    } else {
       Serial.println("Motor desligado");
     }
   }
 }
 
-void alterna_motor()
-{
-  motorLigado = !motorLigado; // alterna entre ligado e desligado
+void alterna_motor() {
+  motorLigado = !motorLigado; 
   if (motorLigado) {
     Serial.println("Motor ligado");
   } else {
@@ -60,14 +54,23 @@ void alterna_motor()
   }
 }
 
+void verificaBotao() {
+  buttonState = digitalRead(BUTTON_PIN); 
+
+  if (buttonState != lastButtonState) {
+    if (buttonState == LOW) {
+      alterna_motor();
+    }
+    delay(50); 
+  }
+  lastButtonState = buttonState; 
+}
+
 void setup() {
   inicializa_motor();
 }
 
 void loop() {
+  verificaBotao(); 
   atualiza_motor();
-  // Simulando alternância de estado do motor a cada 5 segundos (5000 ms)
-  if (millis() % 5000 < 1000) { 
-    alterna_motor();
-  }
 }
