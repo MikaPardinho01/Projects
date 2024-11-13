58dd626e9ac62f95c885ccc4edca8437749dbadb
Mikaelly Pardinho <168122231+MikaPardinho01@users.noreply.github.com>
2024-11-13 17:13:55 -0300

    teste do tempo com senha

--- Esteira/src/main.cpp
+++ Esteira/src/main.cpp
@@ -1,76 +1,43 @@
-// *===== BIBLIOTECAS ======
 #include <Arduino.h>
-#include <Stepper.h> // incluir motor de passo
 
-const int stepsPerRevolution = 2048; // número de passos para uma volta completa de 360 graus
 
-// *===== DEFINICOES ======
-#define IN1 19
-#define IN2 18
-#define IN3 5
-#define IN4 17
-#define BUTTON_PIN 0
+int randNumber;
+unsigned long NovoTempoExtra = 0;
+unsigned long Intervalo_Troca = 0;
+unsigned long Intervalo_Normal = 10000;
+unsigned long Tempo_extra = 30000;
+unsigned long tempo_anterior_token = 0;
+unsigned long troca_Token = 0;
 
-// *===== OBJETOS ========
-Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
-
-unsigned long previousMillis = 0; // variável para armazenar o tempo anterior
-const long interval = 1000; // intervalo de tempo em milissegundos (1 segundo)
-
-bool motorLigado = false; // variável para rastrear o estado do motor
-bool buttonState = false; // estado do botão
-bool lastButtonState = false; // último estado do botão
-
-void inicializa_motor() {
-  // define a velocidade em 5 rpm
-  myStepper.setSpeed(5);
-  Serial.begin(115200);
-  Serial.println("Motor desligado");
-
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // Configura o botão como entrada com resistor de pull-up interno
+void Inicializa_senha()
+{
+    randomSeed(timeStamp());
 }
 
-void atualiza_motor() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-
-    if (motorLigado) {
-      Serial.println("Motor ligado - sentido horário");
-      myStepper.step(stepsPerRevolution);
-    } else {
-      Serial.println("Motor desligado");
+int gera_senha()
+{
+    Reset_user();
+    unsigned long tempo_atual = millis();
+    if (tempo_atual - tempo_anterior_token >= troca_Token)
+    {
+        if (tempo_anterior_token != Intervalo_Normal)
+        troca_Token = Intervalo_Normal;
+        tempo_anterior_token = tempo_atual;
+        randNumber = random(1000, 10000);
+        Serial.print("\n");
+        Serial.printf("Nova Senha: %d\n", randNumber);
     }
-  }
+    return randNumber;
 }
 
-void alterna_motor() {
-  motorLigado = !motorLigado; 
-  if (motorLigado) {
-    Serial.println("Motor ligado");
-  } else {
-    Serial.println("Motor desligado");
-  }
-}
-
-void verificaBotao() {
-  buttonState = digitalRead(BUTTON_PIN); 
-
-  if (buttonState != lastButtonState) {
-    if (buttonState == LOW) {
-      alterna_motor();
+void tempo_extra()
+{
+    if (troca_Token != Tempo_extra)
+    {
+        tempo_anterior_token = millis();
+        troca_Token = Tempo_extra;
+        Serial.print("\n");
+        Serial.printf("Senha estendida por %d segundos", Tempo_extra / 1000);
+        Serial.print("\n");
     }
-    delay(50); 
-  }
-  lastButtonState = buttonState; 
-}
-
-void setup() {
-  inicializa_motor();
-}
-
-void loop() {
-  verificaBotao(); 
-  atualiza_motor();
 }
