f99ee57e968f30c1ff7fafb06bc14765d7745d37
MikaPardinho01 <amikaelly878@gmail.com>
2024-12-05 08:21:45 -0300

    implementacao do segundo servo

--- Complet_code/src/saidas.cpp
+++ Complet_code/src/saidas.cpp
@@ -1,42 +1,33 @@
 #include <FastLED.h>
-#include "saidas.h"
 
-const int LED_PIN = 4;
-const int NUM_LEDS = 2;
-const int LED_PIN_COLOR = 18;
-const int NUM_LEDS_COLOR = 12;
+const int LED_PIN = 4;       
+const int NUM_LEDS_TOTAL = 12; 
 
 #define BRIGHTNESS 255
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
 
-CRGB leds[NUM_LEDS];
-CRGB leds_color[NUM_LEDS_COLOR];
+CRGB leds[NUM_LEDS_TOTAL]; 
 
 unsigned long intervalo_led = 0;
 unsigned long anterior_tempo_led = 1000;
 bool isRed = true;
 
+
+const int NUM_LEDS_MAIN = 4;  
+const int NUM_LEDS_COLOR = 8; 
+const int COLOR_START_INDEX = NUM_LEDS_MAIN; 
+
 void inicializa_leds()
 {
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS_TOTAL);
   FastLED.setBrightness(BRIGHTNESS);
   atualiza_apagado();
 }
 
-void inicializa_leds_color()
-{
-  FastLED.addLeds<LED_TYPE, LED_PIN_COLOR, COLOR_ORDER>(leds_color, NUM_LEDS_COLOR);
-  FastLED.setBrightness(BRIGHTNESS);
-  for (int i = 0; i < NUM_LEDS_COLOR; i++)
-  {
-    leds_color[i] = CRGB::Red;
-  }
-  FastLED.show();
-}
 void atualiza_branco()
 {
-  for (int i = 0; i < NUM_LEDS; i++)
+  for (int i = 0; i < NUM_LEDS_MAIN; i++)
   {
     leds[i] = CRGB::White;
   }
@@ -45,9 +36,18 @@ void atualiza_branco()
 
 void atualiza_apagado()
 {
-  for (int i = 0; i < NUM_LEDS; i++)
+  for (int i = 0; i < NUM_LEDS_MAIN; i++)
   {
     leds[i] = CRGB::Black;
+  }
+  FastLED.show();
+}
+
+void inicializa_leds_color()
+{
+  for (int i = COLOR_START_INDEX; i < NUM_LEDS_TOTAL; i++)
+  {
+    leds[i] = CRGB::Red;
   }
   FastLED.show();
 }
@@ -60,14 +60,13 @@ void atualiza_leds_color()
     intervalo_led = currentMillis;
     if (isRed)
     {
-      leds_color[0] = CRGB::Red;
-      leds_color[1] = CRGB::Yellow;
+      leds[COLOR_START_INDEX] = CRGB::Red;
+      leds[COLOR_START_INDEX + 1] = CRGB::Yellow;
     }
-
     else
     {
-      leds_color[0] = CRGB::Yellow;
-      leds_color[1] = CRGB::Red;
+      leds[COLOR_START_INDEX] = CRGB::Yellow;
+      leds[COLOR_START_INDEX + 1] = CRGB::Red;
     }
 
     isRed = !isRed;
