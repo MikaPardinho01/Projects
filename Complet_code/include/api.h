125a9d04369b5724eee3ca881b77cbab490962b7
Mikaelly Pardinho <168122231+MikaPardinho01@users.noreply.github.com>
2024-12-10 17:12:32 -0300

    implementacao do site

--- Complet_code/include/api.h
+++ Complet_code/include/api.h
@@ -1,2 +1,26 @@
-const char *host = 'localhost';
-const int *port = 9000;
+<?php
+
+include_once './BancoDeDados.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $uid = $_POST['uid'];
+
+    if (!empty($uid)) {
+        $db = new BancoDeDados();
+        $conexao = $db->iniciarConexao();
+
+        $query = "INSERT INTO produto (uid) VALUES (:uid)";
+        $stmt = $conexao->prepare($query);
+        $stmt->bindParam(':uid', $uid);
+
+        if ($stmt->execute()) {
+            echo json_encode(['success' => true, 'message' => 'UID salvo com sucesso.']);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Erro ao salvar o UID.']);
+        }
+    } else {
+        echo json_encode(['success' => false, 'message' => 'UID não pode estar vazio.']);
+    }
+} else {
+    echo json_encode(['success' => false, 'message' => 'Método não permitido.']);
+}
